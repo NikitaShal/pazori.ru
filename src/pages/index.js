fb861e108885bf0540ae24e46f988bd2c5219d5b
Nikitashal <shalomyshka@gmail.com>
2020-09-08 15:53:31 +0300

    carousel buttons started and header desc fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,70 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import Img from 'gatsby-image';
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-// import logo from '../images/logo.svg'
 
-const IndexPage = () => (
+import "../css/multilink.css"
+
+import logo from '../images/logo-pazori.svg'
+
+const IndexPage = props => (
   <Layout>
-    <SEO title="Главная" />
-    <p>одежда и аксессуары из Мурманска</p>
-    <Link to="/multilink/">Go to page 2</Link> <br />
+	<div class="container">
+
+	    <SEO title="Привет" />
+
+	    {/* <Img fluid={props.data.imageOne.childImageSharp.fluid} /> */}
+
+	    <div class="header">
+	    	<div style={{ maxWidth: `100px` }}>
+		      <img src={logo} />
+		    </div>
+		    <p class="description">одежда и аксессуары из Мурманска</p>
+		    <div class="brand-line"></div>
+			</div>
+
+			<div class="grid">
+
+					<div class="item">
+						<Img fluid={props.data.productOne.childImageSharp.fluid} />
+					</div>
+					<div class="item">
+						<Img class="" fluid={props.data.productTwo.childImageSharp.fluid} />
+					</div>
+
+			</div>
+
+			<div class="footer">
+					<button class="optombuttom">ЗВОНОК</button>
+					<button class="optombuttom">ПОЧТА</button>
+
+					<button class="merchbutton">МЕРЧ</button>
+					<button class="optombuttom">ОПТОМ</button>
+			</div>
+    </div>
   </Layout>
 )
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    productOne: file(relativePath: { eq: "white.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 300, maxHeight: 330, quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    productTwo: file(relativePath: { eq: "black.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 300, maxHeight: 330, quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
