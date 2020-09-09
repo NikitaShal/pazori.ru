d862955807bfea432208b86a7f90988e42f4d743
Nikitashal <shalomyshka@gmail.com>
2020-09-09 19:56:36 +0300

    yandex metrica fixed and replaced

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,17 +37,10 @@ module.exports = {
   {
     resolve: `gatsby-plugin-yandex-metrika`,
     options: {
-      // The ID of yandex metrika.
       trackingId: 67178128,
-      // Enabled a webvisor. The default value is `false`.
       webvisor: true,
-      // Enables tracking a hash in URL. The default value is `false`.
       trackHash: true,
-      // Defines where to place the tracking script - `false` means before body (slower loading, more hits)
-      // and `true` means after the body (faster loading, less hits). The default value is `false`.
-      afterBody: true,
-      // Use `defer` attribute of metrika script. If set to `false` - script will be loaded with `async` attribute.
-      // Async enables earlier loading of the metrika but it can negatively affect page loading speed. The default value is `false`.
+      afterBody: false,
       defer: false,
     },
   },
