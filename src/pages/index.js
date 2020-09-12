0b95c3852ba55d05bb5432b4d88f981e935abb55
Nikitashal <shalomyshka@gmail.com>
2020-09-12 08:58:21 +0300

    framer motion testing without

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import Img from 'gatsby-image';
 
-// import { motion } from "framer-motion"
+ import { motion } from "framer-motion"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
